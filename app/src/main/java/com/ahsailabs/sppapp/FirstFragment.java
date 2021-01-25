3253c6503072353f1beb804884670231e73d31e7
Ahmad Saifullah <ahsai001@gmail.com>
2021-01-25 20:55:44 +0700

    first commit

---
+++ app/src/main/java/com/ahsailabs/sppapp/FirstFragment.java
@@ -0,0 +1,168 @@
+package com.ahsailabs.sppapp;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+import app.akexorcist.bluetotohspp.library.BluetoothState;
+import app.akexorcist.bluetotohspp.library.DeviceList;
+
+public class FirstFragment extends Fragment {
+    BluetoothSPP bt;
+    EditText etMessage;
+    TextView tvMessage;
+    String clientName = "";
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        etMessage = view.findViewById(R.id.etMessage);
+        tvMessage = view.findViewById(R.id.tvMessage);
+        view.findViewById(R.id.btnSelectDevices).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                 */
+                if(bt.getServiceState() == BluetoothState.STATE_CONNECTED) {
+                    bt.disconnect();
+                } else {
+                    bt.startDiscovery();
+                    Intent intent = new Intent(getActivity(), DeviceList.class);
+                    intent.putExtra("bluetooth_devices", "Bluetooth devices");
+                    intent.putExtra("no_devices_found", "No device");
+                    intent.putExtra("scanning", "Scanning");
+                    intent.putExtra("scan_for_devices", "Search");
+                    intent.putExtra("select_device", "Select");
+                    startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE);
+                }
+            }
+        });
+
+        view.findViewById(R.id.btnSend).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String info = etMessage.getText().toString();
+                bt.send(info,true);
+                etMessage.setText("");
+                showInfo("Me : "+info);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        bt = new BluetoothSPP(getActivity());
+        bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() {
+            public void onDataReceived(byte[] data, String message) {
+                showInfo(clientName+" : "+message);
+            }
+        });
+        bt.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener() {
+            public void onDeviceConnected(String name, String address) {
+                // Do something when successfully connected
+                clientName = ""+name+"("+address+")";
+                showInfo("onDeviceConnected "+""+name+"("+address+")");
+            }
+
+            public void onDeviceDisconnected() {
+                // Do something when connection was disconnected
+                showInfo("onDeviceDisconnected");
+            }
+
+            public void onDeviceConnectionFailed() {
+                // Do something when connection failed
+                showInfo("onDeviceConnectionFailed");
+            }
+        });
+        bt.setBluetoothStateListener(new BluetoothSPP.BluetoothStateListener() {
+            public void onServiceStateChanged(int state) {
+                if(state == BluetoothState.STATE_CONNECTED) {
+                    // Do something when successfully connected
+                    showInfo("STATE_CONNECTED");
+                } else if(state == BluetoothState.STATE_CONNECTING) {
+                    // Do something while connecting
+                    showInfo("STATE_CONNECTING");
+                } else if(state == BluetoothState.STATE_LISTEN) {
+                    // Do something when device is waiting for connection
+                    showInfo("STATE_LISTEN");
+                } else if(state == BluetoothState.STATE_NONE) {
+                    // Do something when device don't have any connection
+                    showInfo("STATE_NONE");
+                }
+            }
+        });
+
+        if(!bt.isBluetoothAvailable()) {
+            // any command for bluetooth is not available
+            showInfo("bluetooth is not available");
+        } else {
+            if (!bt.isBluetoothEnabled()) {
+                Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(intent, BluetoothState.REQUEST_ENABLE_BT);
+            } else {
+                if(!bt.isServiceAvailable()) {
+                    bt.setupService();
+                    bt.startService(BluetoothState.DEVICE_ANDROID);
+                    //setup();
+                }
+            }
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
+            if(resultCode == Activity.RESULT_OK)
+                bt.connect(data);
+        } else if(requestCode == BluetoothState.REQUEST_ENABLE_BT) {
+            if(resultCode == Activity.RESULT_OK) {
+                bt.setupService();
+                bt.startService(BluetoothState.DEVICE_ANDROID);
+                //setup();
+            } else {
+                // Do something if user doesn't choose any device (Pressed back)
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        bt.stopService();
+    }
+
+    StringBuilder infoBuilder = new StringBuilder();
+    private void showInfo(String info){
+        infoBuilder.append(info);
+        infoBuilder.append("\n");
+        tvMessage.setText(infoBuilder.toString());
+    }
+}
